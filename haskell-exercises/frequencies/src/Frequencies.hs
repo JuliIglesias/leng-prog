67bb35a4a4b13ec6dff0994b1101185470ecbd28
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-09-08 20:02:37 -0300

    -

--- haskell-exercises/frequencies/src/Frequencies.hs
+++ haskell-exercises/frequencies/src/Frequencies.hs
@@ -6,14 +6,38 @@ import Data.Tuple(swap)
 
 type Frequency = (Int, Char)
 
-frequencies::String -> [Frequency]
-frequencies = error "Implement it"
+frequencies :: String -> [Frequency]
+frequencies str = insertionSort $ map swap $ Map.toList $ frequencyMap str
 
-frequencyMap::(Ord a) => [a] -> Map a Int
-frequencyMap = error "Implement it"
+frequencyMap :: (Ord a) => [a] -> Map a Int
+frequencyMap = foldr (\x acc -> Map.insertWith (+) x 1 acc) Map.empty
 
-insert::(Ord a) => a -> [a] -> [a]
-insert = error "Implement it"
+insert :: (Ord a) => a -> [a] -> [a]
+insert x [] = [x]
+insert x (y:ys)
+  | x <= y    = x : y : ys
+  | otherwise = y : insert x ys
 
+-- Sort a list using insertion sort
 insertionSort :: (Ord a) => [a] -> [a]
-insertionSort = error "Implement it"
+insertionSort = foldr insert []
+
+-- Chau jli :D atte. Eze Quiel
+
+import System.Environment (getArgs)
+import System.IO (readFile)
+import Data.List (sortBy)
+import Data.Ord (comparing)
+import Frequencies (frequencies)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  if null args
+    then putStrLn "Error: No file name provided."
+    else do
+      let fileName = head args
+      content <- readFile fileName
+      let freqList = frequencies content
+      let sortedFreqList = reverse $ sortBy (comparing fst) freqList
+      mapM_ (\(count, char) -> putStrLn $ show char ++ ": " ++ show count) sortedFreqList
--- haskell-exercises/huffman/src/Huffman.hs
+++ haskell-exercises/huffman/src/Huffman.hs
@@ -1,19 +1,43 @@
-module Huffman  (huffmanTrie, encode, decode, Trie(..), Bit(..)) where
+module Huffman (huffmanTrie, encode, decode, Trie(..), Bit(..)) where
 
-import qualified Data.Map as M 
+import qualified Data.Map as M
+import Data.List (sortBy, insertBy)
+import Data.Ord (comparing)
+
+frequencyMap :: (Ord a) => [a] -> M.Map a Int
+frequencyMap = foldr (\x acc -> M.insertWith (+) x 1 acc) M.empty
 
 data Bit = F | T deriving (Eq, Show)
-type Bits = [Bit]  
+type Bits = [Bit]
 
 data Trie a = Empty
             | Leaf a
             | Trie a :-: Trie a deriving (Eq, Show, Ord)
 
-huffmanTrie::String -> Trie Char
-huffmanTrie input =  error "Implement it"
+huffmanTrie :: String -> Trie Char
+huffmanTrie "" = Empty
+huffmanTrie input = buildTrie $ map (\(c, f) -> (f, Leaf c)) $ M.toList $ frequencyMap input
+  where
+    buildTrie [(_, trie)] = trie
+    buildTrie ((f1, t1):(f2, t2):rest) = buildTrie $ insertBy (comparing fst) (f1 + f2, t1 :-: t2) rest
+    buildTrie [] = Empty
 
 encode :: String -> Trie Char -> Bits
-encode input code = error "Implement it"
-  
-decode::Bits -> Trie Char -> String
-decode bits trie = error "Implement it"
+encode input trie = concatMap (charToBits M.!) input
+  where
+    charToBits = buildMap trie M.empty []
+    buildMap (Leaf c) m bits = M.insert c bits m
+    buildMap (l :-: r) m bits = buildMap r (buildMap l m (bits ++ [F])) (bits ++ [T])
+    buildMap Empty m _ = m
+
+decode :: Bits -> Trie Char -> String
+decode bits trie = decodeBits bits trie
+  where
+    decodeBits [] _ = []
+    decodeBits bs t = case decodeChar bs t of
+                        (c, rest) -> c : decodeBits rest t
+    decodeChar (F:bs) (l :-: _) = decodeChar bs l
+    decodeChar (T:bs) (_ :-: r) = decodeChar bs r
+    decodeChar [] (Leaf c) = (c, [])
+    decodeChar bs (Leaf c) = (c, bs)
+    decodeChar _ Empty = error "Invalid Trie structure"
